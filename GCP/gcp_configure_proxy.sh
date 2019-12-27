1162b3cface70202080cdb3a7e3aa0ff3b8ad989
Laura Jordana <lauram93@gmail.com>
2019-12-26 18:52:09 -0800

    GCP scripts

---
+++ GCP/gcp_configure_proxy.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+new_instance_name=$1
+
+if [ -z $new_instance_name ]; then
+  echo "Usage: $0 <instance_name>"
+  echo "No instance provided, exiting"
+  exit 1
+fi
+
+# get external IP
+
+# external_ip=`gcloud compute instances describe $new_instance_name .... tbd
+external_ip=`gcloud compute instances describe $new_instance_name --format=json | jq -r .networkInterfaces[].accessConfigs[].natIP`
+echo $external_ip
+destination_ip="172.31.0.202"
+
+# sleep before logging in to make sure it's come up all the way
+sleep 10
+
+# this assumes SSH keys are set up for the following user
+user="laura"
+
+# check if it's accessible via ssh
+status=1
+attempts=0
+while [ $status != 0 ]; do
+  attempts=$((i+1))
+  if [ $attempts == 5 ]; then
+    echo "Quitting after 5th attempt, configure VM manually."
+    exit 1
+  fi
+  ssh $user@$external_ip 'hostname'
+  status=$?
+  echo "status = $status"
+done
+
+# change root password so the instance can be accessed via serial console after forwarding is enabled
+# set iptables nat rules to forward to $destination_ip
+
+ssh $user@$external_ip "printf \"%s\n\" nutanix/4u nutanix/4u | sudo passwd ; sudo iptables -t nat -A PREROUTING -j DNAT --to-destination $destination_ip ; sudo iptables -t nat -A POSTROUTING -j MASQUERADE ; sudo sysctl net.ipv4.ip_forward=1"
+
+# sudo iptables -t nat -A PREROUTING -j DNAT --to-destination $destination_ip
+# sudo iptables -t nat -A POSTROUTING -j MASQUERADE
+# sudo sysctl net.ipv4.ip_forward=1
---
+++ GCP/gcp_create_prismpro_proxy.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+set -x
+
+# Script to create GCP instance in the same subnet as a given instance (identified by its external IP)
+
+########################
+# user defined variables
+########################
+
+# project to use
+project="testdrive02"
+
+# external IP of a given instance
+external_ip="34.83.7.88"
+
+# zone the instance is in
+zone="us-west1-c"
+
+# new instance name prefix
+new_instance_prefix="prismproproxy"
+
+# disk image to use
+disk_image="ubuntu-minimal-1604-xenial-v20191217"  # get image list from gcloud compute images list
+disk_image_project="ubuntu-os-cloud"
+
+##############
+# script start
+##############
+
+# set project
+gcloud config set project $project
+
+# set zone
+gcloud config set compute/zone $zone
+
+# get existing instance info
+existing_instance=`gcloud compute instances list | grep $external_ip | awk '{print $1}'`
+
+if [ -z $existing_instance ]; then
+  echo "Instance with external_ip $external_ip not found, exiting."
+  exit 1
+else
+  echo "Found instance $existing_instance"
+fi
+
+# get existing instance UUID
+instance_uuid=`echo $existing_instance | cut -f4 -d-`
+
+# set new instance name based on existing UUID
+new_instance_name=$new_instance_prefix-$instance_uuid
+
+# get subnet from existing instance
+network=`gcloud compute instances describe $existing_instance --format="json" | jq -r ".networkInterfaces[].network" | cut -f10 -d/`
+subnet="$network-subnet"
+
+# create instance within this VPC
+gcloud compute instances create $new_instance_name --image $disk_image --image-project $disk_image_project --network $network --subnet $subnet --can-ip-forward
+
+# enable connecting to serial ports
+gcloud compute instances add-metadata $new_instance_name --metadata serial-port-enable=true
+
+# update firewall rules
+gcloud compute firewall-rules create prismproproxy-$instance_uuid --allow tcp:80,tcp:8080 --network $network
+
+# configure the new host with firewall rules
+./gcp_configure_proxy.sh $new_instance_name
