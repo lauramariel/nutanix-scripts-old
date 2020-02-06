e5a8f4514eb83b19b0e1699a9e783b3fce43f1d7
Laura Jordana <lauram93@gmail.com>
2020-02-05 20:37:18 -0800

    Script to create file share

---
+++ ONCE/files_create_share.py
@@ -0,0 +1,67 @@
+#!/usr/local/bin/python3
+#
+# Script to create a SMB share on an existing file server
+# Author: Laura Jordana
+# Date: 2/5/2020
+# Tested on AOS 5.11
+
+import json
+import requests
+from requests.auth import HTTPBasicAuth
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+# User defined variables
+PE_IP = "10.38.6.70"
+PC_IP = "10.38.6.73"
+PE_AUTH_TYPE = HTTPBasicAuth("admin", 'nx2Tech265!')
+PC_AUTH_TYPE = HTTPBasicAuth("admin", 'nx2Tech265!')
+SUBNET_NAME = "default-net"
+FILES_VERSION = "3.6.1.1"
+FILESERVER_NAME = "filesvr1" # File server to create the share on
+SHARE_NAME = "MyShare" # Desired name of share
+PROTOCOL = "SMB" # SMB or NFS
+
+
+# Set required variables
+HEADERS = {'Content-type': 'application/json'}
+
+def get_fileserver_uuid():
+	get_fs_url = "https://{}:9440/PrismGateway/services/rest/v1/vfilers/?searchString={}".format(PE_IP, str(FILESERVER_NAME))
+	#print(get_fs_url)
+	fs_details = requests.get(get_fs_url, auth=PE_AUTH_TYPE, headers=HEADERS, data='{}', verify=False)
+	#print(fs_details)
+	if fs_details.ok:
+		parsed_fs_details = json.loads(fs_details.content)
+		fs_uuid = str(parsed_fs_details["entities"][0]["uuid"])
+		return fs_uuid
+	return 
+
+def create_share(fs_uuid):
+	url = "https://{}:9440/PrismGateway/services/rest/v1/vfilers/{}/shares".format(PE_IP, str(fs_uuid))
+	#print(url)
+	payload = {
+	    "name": SHARE_NAME,
+	    "fileServerUuid": fs_uuid,
+	    "enablePreviousVersion": "true",
+	    "windowsAdDomainName": "ntnxlab.local",
+	    "description": "",
+	    "maxSizeGiB": 0,
+	    "protocolType": PROTOCOL,
+	    "secondaryProtocolType": "NONE",
+	    "sharePath": "",
+	    "isNestedShare": "false",
+	    "enableAccessBasedEnumeration": "true",
+	    "enableSmb3Encryption": "false",
+	    "shareType": "GENERAL"
+	}
+
+	print(json.dumps(payload))
+	resp = requests.post(url, auth=PE_AUTH_TYPE, headers=HEADERS, data=json.dumps(payload), verify=False)
+	print(resp)
+
+if __name__ == "__main__":
+	fileserver_uuid = get_fileserver_uuid()
+	#print(fileserver_uuid)
+	create_share(fileserver_uuid)
