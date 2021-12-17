e94ffa4c0b2b71622c500b2250d1e3f672508d8a
Laura Jordana <laura@nutanix.com>
2021-12-17 12:19:54 -0800

    add README, config.json, requirements for venv

--- nutanix/vm_report_csv/vm_report.py
+++ nutanix/vm_report_csv/vm_report.py
@@ -12,10 +12,10 @@ import argparse
 import getpass
 import re
 from base64 import b64encode
-from requests.auth import HTTPBasicAuth
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+
 
 class RequestParameters:
     """
@@ -34,6 +34,8 @@ class RequestParameters:
             f"username={self.username},"
             f"password={self.password},"
         )
+
+
 class RequestResponse:
     """
     Class to hold the response from our Request
@@ -92,8 +94,8 @@ class RESTClient:
         }
         try:
             api_request = requests.get(
-                    self.params.uri, headers=headers, timeout=30, verify=False
-                )
+                self.params.uri, headers=headers, timeout=30, verify=False
+            )
             # if no exceptions occur here, we can process the response
             response.code = api_request.status_code
             response.message = "Request submitted successfully."
@@ -102,7 +104,7 @@ class RESTClient:
         except ValueError:
             # handle when our APIs do not return a JSON body
             response.code = api_request.status_code
-            response.message = f"ValueError was caught."
+            response.message = "ValueError was caught."
             response.details = "N/A"
         except requests.exceptions.ConnectTimeout:
             # timeout while connecting to the specified IP address or FQDN
@@ -112,7 +114,7 @@ class RESTClient:
         except urllib3.exceptions.ConnectTimeoutError:
             # timeout while connecting to the specified IP address or FQDN
             response.code = -96
-            response.message = f"Connection has timed out."
+            response.message = "Connection has timed out."
             response.details = "urllib3.exceptions.ConnectTimeoutError"
         except requests.exceptions.MissingSchema:
             # potentially bad URL
@@ -126,10 +128,13 @@ class RESTClient:
             response.details = _e
 
         return response
+
+
 class NameFilter(logging.Filter):
     """
     Class to contextually change the log based on the VM being processed
     """
+
     def __init__(self, entity_name):
         self.entity_name = entity_name
 
@@ -137,19 +142,17 @@ class NameFilter(logging.Filter):
         record.entity_name = self.entity_name
         return True
 
+
 def api_request(url, pe_ip, pe_user, pe_password):
     """Create a new entity via a v3 post call, return the response"""
 
     # Make the API call
-    parameters = RequestParameters(
-        uri=url,
-        username=pe_user,
-        password=pe_password
-    )
+    parameters = RequestParameters(uri=url, username=pe_user, password=pe_password)
     rest_client = RESTClient(parameters)
     resp = rest_client.request()
 
     return resp
+
 
 def custom_log(entity_name):
     """
@@ -161,16 +164,19 @@ def custom_log(entity_name):
     if not logger.hasHandlers():
         logname = f"{entity_name}-log-{current_time}.log"
         filelog = logging.FileHandler(filename=logname)
-        formatter = logging.Formatter("%(levelname).1s %(asctime)s [%(entity_name)s] %(message)s")
+        formatter = logging.Formatter(
+            "%(levelname).1s %(asctime)s [%(entity_name)s] %(message)s"
+        )
         filelog.setFormatter(formatter)
         logger.setLevel(logging.INFO)
         print(f"Logging to {logname}")
         logger.addHandler(filelog)
     return logger
 
+
 def get_cluster_name(pe_ip, pe_user, pe_password):
     """
-    Given a Prism Element IP, return the name of the cluster 
+    Given a Prism Element IP, return the name of the cluster
     """
     cluster_url = f"https://{pe_ip}:9440/PrismGateway/services/rest/v1/clusters"
     resp = api_request(cluster_url, pe_ip, pe_user, pe_password)
@@ -181,6 +187,7 @@ def get_cluster_name(pe_ip, pe_user, pe_password):
         cluster_name = i["name"]
 
     return cluster_name
+
 
 def main(pe_ip, pe_user, pe_password, report_name, duration):
 
@@ -194,13 +201,13 @@ def main(pe_ip, pe_user, pe_password, report_name, duration):
             filename = f"{cluster_name}-vm-report-{current_time}.csv"
     else:
         filename = report_name
-    f = open(filename, "w")
+    report = open(filename, "w")
 
     # URL for Stats of All VM Stats
     url = f"https://{pe_ip}:9440/PrismGateway/services/rest/v1/vms"
 
     # remove any whitespace from cluster_name as it will be used in the log filename
-    cluster_name = re.sub(r"\s+", '-', cluster_name)
+    cluster_name = re.sub(r"\s+", "-", cluster_name)
     logger = custom_log(cluster_name)
     logger.info(f"Report being written to {filename}")
     logger.info(f"{url}")
@@ -209,7 +216,7 @@ def main(pe_ip, pe_user, pe_password, report_name, duration):
     resp = api_request(url, pe_ip, pe_user, pe_password)
     results = resp.json
     try:
-        total_vms = results.get('metadata', {}).get('grandTotalEntities')
+        total_vms = results.get("metadata", {}).get("grandTotalEntities")
     except Exception as e:
         logger.error(f"Error when parsing metadata: {e}")
         sys.exit(1)
@@ -218,48 +225,36 @@ def main(pe_ip, pe_user, pe_password, report_name, duration):
     if duration:
         logger.info(f"Getting VM metrics for last {duration} days")
 
-    # this dict maps the desired attribute as obtained from the API
-    # with the display name that will be used in the report
-    # clusterUuid is coming from a different call but we want it to be
-    # in the second column, so including it here as we'll loop
-    # through this later when writing to the file
-    attributes = {
-        "vmName": "Name",
-        "clusterUuid": "Parent Cluster",
-        "numVCpus": "Provisioned vCPUs",
-        "memoryCapacityInBytes": "Memory Capacity",
-        "diskCapacityInBytes": "Disk Capacity",
-    }
+    # setup a variable that can be used to store our JSON configuration
+    config_json = {}
 
-    # this dict maps the desired metric as obtained from the API
-    # with the display name that will be used in the report
-    metrics = {
-        "hypervisor_cpu_usage_ppm": "CPU Usage (%)",
-        "guest.memory_usage_bytes": "Memory Consumed (GiB)",
-        "memory_usage_ppm": "Memory Usage (%)",
-        "controller_user_bytes": "Guest File System Utilization (%)",
-        "hypervisor_num_received_bytes": "Network - Data Receive Rate (KBps)",
-        "hypervisor_num_transmitted_bytes": "Network - Data Transmit Rate (KBps)",
-    }
+    # grab and decode the category details from the included JSON file
+    with open("./config.json", "r") as config:
+        config_json = json.loads(config.read())
+
+    # these dicts map the desired attributes and metrics
+    # with the display names that will be used in the report
+    attributes = config_json["config"]["attributes"]
+    metrics = config_json["config"]["metrics"]
 
     # Headings
     for attr in attributes.values():
-        f.write(f"{attr},")
+        report.write(f"{attr},")
     for metric in metrics.values():
-        f.write(f"{metric} (Max),")
-        f.write(f"{metric} (Average),")
+        report.write(f"{metric} (Max),")
+        report.write(f"{metric} (Average),")
 
-    f.write("\n")
+    report.write("\n")
 
     # Content
     for vm in results["entities"]:
         vm_name = vm["vmName"]
         logger = custom_log(vm_name)
         vm_uuid = vm["uuid"]
- 
+
         for attr, display_name in attributes.items():
             if attr == "clusterUuid":
-                f.write(f"{cluster_name}" + ",")
+                report.write(f"{cluster_name}" + ",")
             elif "Bytes" in attr:
                 # 1073741824 bytes = 1 GiB
                 if vm[f"{attr}"]:
@@ -267,25 +262,29 @@ def main(pe_ip, pe_user, pe_password, report_name, duration):
                 else:
                     value_in_gib = 0
                 logger.info(f"{display_name}: {value_in_gib} GiB")
-                f.write(f"{value_in_gib} GiB,")
+                report.write(f"{value_in_gib} GiB,")
             else:
                 attribute_value = vm[f"{attr}"]
                 logger.info(f"{display_name}: {attribute_value}")
-                f.write(str(attribute_value) + ",")
+                report.write(str(attribute_value) + ",")
 
         logger.info(log_delimiter)
 
         for vm_metric, display_name in metrics.items():
-            # query the API for the specified duration for the specific VM and metric, then calculate the max and average
+            # query the API for the specified duration for the specific VM and
+            # metric, then calculate the max and average
 
             if duration == 0:
                 metric_url = f"{url}/{vm_uuid}/stats/?metrics={vm_metric}"
             else:
                 startTimeInUsecs = int(
-                    (datetime.datetime.now() - datetime.timedelta(days=int(duration))).timestamp()
+                    (
+                        datetime.datetime.now() - datetime.timedelta(days=int(duration))
+                    ).timestamp()
                     * 1000000
                 )
-                metric_url = f"{url}/{vm_uuid}/stats/?metrics={vm_metric}&startTimeInUsecs={startTimeInUsecs}"
+                metric_url = f"{url}/{vm_uuid}/stats/?metrics={vm_metric}&" + \
+                    f"startTimeInUsecs={startTimeInUsecs}"
 
             metric_resp = api_request(metric_url, pe_ip, pe_user, pe_password)
             metric_results = metric_resp.json
@@ -321,8 +320,11 @@ def main(pe_ip, pe_user, pe_password, report_name, duration):
                                 max_value = 0
                                 average = 0
                         elif "ppm" in vm_metric:
-                            # reported in parts per million, divide by 1e6 and multiply by 100 to get %
-                            max_value = float("{:.2f}".format((max_value / 1000000) * 100))
+                            # reported in parts per million, divide by 1e6 and
+                            # multiply by 100 to get %
+                            max_value = float(
+                                "{:.2f}".format((max_value / 1000000) * 100)
+                            )
                             average = float("{:.2f}".format((average / 1000000) * 100))
                         elif "memory_usage_bytes" in vm_metric:
                             # convert bytes to GiB - divide by 1073741824
@@ -335,23 +337,24 @@ def main(pe_ip, pe_user, pe_password, report_name, duration):
                         logger.info(f"Max Value after conversion: {max_value}")
                         logger.info(f"Average Value after conversion: {average}")
                         logger.info(log_delimiter)
-                        f.write(f"{max_value},")
-                        f.write(f"{average},")
+                        report.write(f"{max_value},")
+                        report.write(f"{average},")
                     else:
                         # Log a warning if no values were returned
                         logger.warning(message)
                         logger.info(log_delimiter)
                         max_value = 0
                         average = 0
-                        f.write("0,0,")
+                        report.write("0,0,")
             else:
                 logger.error(f"Error fetching stat details for {vm_name}")
-        f.write("\n")
+        report.write("\n")
 
-    f.close()
+    report.close()
 
     print(f"Report written to {filename}")
     sys.exit(0)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
